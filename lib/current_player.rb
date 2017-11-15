d3bd994ec7ca4c57a1b3b87d85d0f4ce89711c22
Phoebe Lau <phoebelau7@gmail.com>
2017-11-15 22:19:46 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,14 +1,18 @@
 
 def turn_count(board)
 counter = 0
-
+board.each do |turn|
+  if turn == "X" or turn == "O"
+    counter += 1
+    end
+end
 end
 
 
 def current_player(board)
-if turn_count % 2 == 0
+if turn_count(board) % 2 == 0
   return "X"
-elsif turn_count % 2 == 1
+elsif turn_count(board) % 2 == 1
   return "O"
 end
 end
